08b53a24366f91a390529ff33efc143f2ebca3ee
Deep687 <deepmankar0@gmail.com>
2026-05-04 15:41:38 +0530

    Intial setup

--- Chattrix-Backend/routes/api.php
+++ Chattrix-Backend/routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return response()->json([
-        'message' => 'Hello from Laravel API',
-        'success' => true,
-    ]);
-});
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
